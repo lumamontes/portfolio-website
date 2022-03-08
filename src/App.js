24677233692447bf50dc295f04e1100c6fdf0322
Luma Montes <lumagoesmontes@gmail.com>
2022-03-07 21:55:13 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router className="App">
+      <Sidebar />
+      <Navbar/>
+    </Router>
   );
 }
 
